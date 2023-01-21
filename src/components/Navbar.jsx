cad450da24c7ccd2dc38594c35ceda472aab8ce0
nurfitriana87 <nurfitrianafina08@gmail.com>
2023-01-21 09:22:35 +0700

    potato-company profile

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="w-screen h-[82px] z-10  fixed drop shadow-lg">
+      <div>
+        <h1 className="text-center bg-white text-lg">ini testing</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
